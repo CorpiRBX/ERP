e1634ce7a70081f7b852f01cc19199f49c713854
CorpiRBX <dmorant@rbxprogramming.com>
2025-01-20 15:38:15 +0100

    Commit timestamp: 2025-01-20 15:38

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
-    import Form from './Form';
+import React, { useState } from "react";
+import Form from "./Form";
 
-    function App() {
-      const [showForm, setShowForm] = useState(false);
+function App() {
+  const [showForm, setShowForm] = useState(false);
 
-      const handleOpenForm = () => {
-        setShowForm(true);
-      };
+  const handleOpenForm = () => {
+    setShowForm(true);
+  };
 
-      const handleCloseForm = () => {
-        setShowForm(false);
-      };
+  const handleCloseForm = () => {
+    setShowForm(false);
+  };
 
-      return (
-        <div>
-          <button onClick={handleOpenForm}>Open Form</button>
-          {showForm && <Form onClose={handleCloseForm} />}
-        </div>
-      );
-    }
+  const state = "onsite";
+  const isMobile = false;
 
-    export default App;
+  return (
+    <div>
+      <button onClick={handleOpenForm}>Open Form</button>
+      {showForm && <Form onClose={handleCloseForm} state={state} isMobile={isMobile}/>}
+    </div>
+  );
+}
+
+export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Form.css";
 import { DateRange } from "react-date-range";
 import { format } from "date-fns";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import "rsuite/dist/rsuite.min.css";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 
-
-const Form = ({ onClose }) => {
+const Form = ({ onClose, state, isMobile }) => {
   const [dateRange, setDateRange] = useState([
     {
       startDate: new Date(),
@@ -22,12 +22,26 @@ const Form = ({ onClose }) => {
     newTime.setHours(newTime.getHours() + 9);
     return newTime;
   });
-  const [time3, setTime3] = useState("01:00");
 
   const [showCalendar, setShowCalendar] = useState(false);
+  const maxDate = state !== "vacaciones" ? new Date() : null;
 
   const handleDateChange = (item) => {
     setDateRange([item.selection]);
+  };
+
+  const handleSelect = () => {
+    const selectedDates = [];
+    const { startDate, endDate } = dateRange[0];
+  
+    // Generar el array de fechas seleccionadas entre startDate y endDate
+    let currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+      selectedDates.push(format(currentDate, "dd/MM/yyyy"));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    console.log(selectedDates);
+    toggleCalendar();
   };
 
   const handleSubmit = (event) => {
@@ -39,26 +53,53 @@ const Form = ({ onClose }) => {
     setShowCalendar(!showCalendar);
   };
 
-  const handleTime1Change = (value) => {
-    if (value) {
-      setTime1(value);
-    }
+  const canvasRef = useRef(null);
+  // Función para limpiar el contenido del Sketch
+  const handleClearClick = () => {
+    canvasRef.current?.clearCanvas();
   };
 
-  const handleTime2Change = (value) => {
-    if (value) {
-      setTime2(value);
-    }
+  const stateToImageMap = {
+    oficina: {
+      img: "../../../src/assets/images/Oficinas.jpg",
+      text: "OFICINA",
+    },
+    onsite: {
+      img: "../../../src/assets/images/Onsite.jpg",
+      text: "ONSITE",
+    },
+    vacaciones: {
+      img: "../../../src/assets/images/Vacaciones.jpg",
+      text: "VACACIONES",
+    },
+    baja: {
+      img: "../../../src/assets/images/Baja.jpg",
+      text: "BAJA",
+    },
   };
 
   return (
     <div className="form-overlay">
-      <div className="form-container">
+      <div className={`form-container ${isMobile ? "isMobile" : "isDesktop"}`}>
         <form onSubmit={handleSubmit}>
           <h2 className="form-title">FICHAJE</h2>
-          <div className="form-box"></div>
+          <div className="form-box">
+            {stateToImageMap[state] ? (
+              <div>
+                <img
+                  src={stateToImageMap[state].img}
+                  className={`form-state-img ${state}`}
+                  alt={stateToImageMap[state].text}
+                />
+              </div>
+            ) : (
+              <p>Estado no válido</p>
+            )}
+          </div>
           {/* OFICINAS */}
-          <label className="form-label">OFICINAS</label>
+          <label className="form-label">{stateToImageMap[state].text}</label>
+          {(state === "onsite" || state === "oficina") && (
+            <>
           <div className="form-group">
             <label htmlFor="departamento" className="form-group-label">
               DEPARTAMENTO
@@ -93,6 +134,8 @@ const Form = ({ onClose }) => {
           <div className="form-box-text">
             <textarea id="observaciones" className="form-textarea"></textarea>
           </div>
+          </>
+          )}
           {/* FECHA */}
           <label className="form-group-label">FECHA</label>
           <div className="form-group date-inputs">
@@ -140,22 +183,26 @@ const Form = ({ onClose }) => {
                   onChange={handleDateChange}
                   moveRangeOnFirstSelection={false}
                   ranges={dateRange}
+                  maxDate={maxDate}
                 />
-                <button
-                  type="button"
-                  className="select-button"
-                  onClick={toggleCalendar}
-                >
-                  Select
-                </button>
+                <div className="form-button-ends">
+                  <button
+                    type="button"
+                    className="select-button"
+                    onClick={handleSelect}
+                  >
+                    Select
+                  </button>
+                </div>
               </div>
             )}
           </div>
 
           {/* TIME PICKER */}
-
+          {(state === "onsite" || state === "oficina") && (
+            <>
           <div className="time-picker-row">
-            <div>
+            <div style={{ marginBottom: "1rem" }}>
               <label className="form-group-label">HORA DE ENTRADA</label>
               <input
                 type="time"
@@ -170,7 +217,11 @@ const Form = ({ onClose }) => {
                 }}
               />
             </div>
-            <div className="time-picker-input">
+            <div
+              className={`time-picker-input ${
+                isMobile ? "isMobile" : "isDesktop"
+              }`}
+            >
               <label className="form-group-label">HORA DE SALIDA</label>
               <input
                 type="time"
@@ -186,20 +237,63 @@ const Form = ({ onClose }) => {
               />
             </div>
           </div>
-          <label className="form-group-label">DESCANSO</label>
-          <div>
-            <input
-              type="time"
-              className="time-input time-3"
-              defaultValue="01:00"
-              onChange={(e) => {
-                console.log("Nuevo valor:", e.target.value);
-              }}
-            />
+          <div style={{ marginBottom: "1rem" }}>
+            <label className="form-group-label">DESCANSO</label>
+            <div>
+              <input
+                type="time"
+                className="time-input time-3"
+                defaultValue="01:00"
+                onChange={(e) => {
+                  console.log("Nuevo valor:", e.target.value);
+                }}
+              />
+            </div>
           </div>
+          </>
+          )}
+          {/* FIRMA */}
           <div>
-            <label className="form-group-label">FIRMA</label>
-            
+            <div style={{ display: "flex" }}>
+              <div>
+                <label className="form-group-label">FIRMA</label>
+              </div>
+              <div className="align-right">
+                <button
+                  className="form-button-reload"
+                  type="button"
+                  onClick={handleClearClick}
+                >
+                  <img
+                    src="../../../src/assets/icons/IconRecarga.png"
+                    className="form-button-icon"
+                    alt="Firma"
+                  />
+                </button>
+              </div>
+            </div>
+            <div className="form-draw-box">
+              <ReactSketchCanvas
+                ref={canvasRef}
+                width="100%"
+                height="150px"
+                canvasColor="transparent"
+                strokeColor="black"
+                strokeWidth={2}
+              />
+            </div>
+          </div>
+          <div className="form-button-ends">
+            <button
+              type="button"
+              className="form-button-cancel"
+              onClick={onClose}
+            >
+              Cancelar
+            </button>
+            <button type="submit" className="form-button-submit">
+              Enviar
+            </button>
           </div>
         </form>
       </div>
