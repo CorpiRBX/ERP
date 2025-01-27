51160055dc9c7460bf256c11b815d38665db561b
RBX\dmorant <dmorant@rbxprogramming.com>
2025-01-27 12:08:04 +0100

    build fix 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import VitePluginHistory from 'vite-plugin-history';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginHistory(),
+  ],
   build: {
     outDir: 'dist',
   },
+  server: {
+    open: true, // opcional, abre el navegador automáticamente
+  },
   resolve: {
     alias: {
-      '@': '/src',  // Agregar un alias para facilitar las rutas
+      '@': '/src',
     },
   },
 });
