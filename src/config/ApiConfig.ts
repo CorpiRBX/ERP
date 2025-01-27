1ea2741c51efabd498e0b685235a4749a05a317c
Alberto <aplaza@rbxprogramming.com>
2025-01-27 12:03:29 +0100

    feat: usar certificado https ssl

--- src/config/ApiConfig.ts
+++ src/config/ApiConfig.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7172/api/', 
+  // baseURL: 'https://localhost:7172/api/', 
+  baseURL: 'https://172.16.143.74:7172/api/', 
+  // baseURL: 'https://127.0.0.1:7172/api/', 
+  // baseURL: 'https://172.16.143.112:7174/api/', 
   headers: {
     'Content-Type': 'application/json',
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: { 
+    https: true, // Habilita HTTPS
+    host: '0.0.0.0', // Permite acceder desde tu red local
+    port: 5173, // Puerto donde corre Vite
+    },
+  plugins: [react(), mkcert()],
 })
